78a93eaebd89c4acbeefa8961e1be2b59dda1aa4
root <root@dtwitter.cloud.compas.cs.stonybrook.edu>
2017-03-23 02:46:16 +0000

    making progress

--- client/dtwitterApp.js
+++ client/dtwitterApp.js
@@ -21,3 +21,36 @@ app.config(function($routeProvider, $locationProvider){
                 });
 	$locationProvider.html5Mode(true);
 });	
+
+app.controller('authController', function($scope, $http, $rootScope, $location){
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
+	
+	$scope.login = function(){
+		$http.post('/auth/login', $scope.user).success(function(data){
+			if(data.state == 'success'){
+				$rootScope.authenticated = true;
+				$rootScope.current_user = data.user.username;
+				$location.path('/');
+			}else{
+				$scope.error_message = data.message;
+			}
+		
+		});
+
+	};
+
+	$scope.register = function(){
+                $http.post('/auth/adduser', $scope.user).success(function(data){
+                        if(data.state == 'success'){
+                                $rootScope.authenticated = true;
+                                $rootScope.current_user = data.user.username;
+                                $location.path('/');
+                        }else{
+                                $scope.error_message = data.message;
+                        }
+
+                });
+
+        };
+});
--- server.js
+++ server.js
@@ -5,7 +5,17 @@ var app		= express();
 var bodyParser 	= require('body-parser');
 var path 	= require('path');
 
-var index 	= require('./routes/index');
+var logger	= require('morgan');
+var session	= require('express-session');
+var passport	= require('passport');
+
+require('./models/models');
+var mongoose	= require('mongoose');
+mongoose.connect('mongodb://localhost/dtwitter');
+
+
+var index       = require('./routes/index');
+var authenticate= require('./routes/authenticate')(passport);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,13 +23,22 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + /client/));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(logger('dev'));
+app.use(session({
+	secret: 'keyboard cat',
+	resave: true,
+        saveUninitialized: true
+}));
 
 app.use('/', index);
+app.use('/auth', authenticate);
 
 var port = process.env.PORT || 8080;
 
 //routes
-
 var router = express.Router();
 
 router.use(function(req, res, next) {
@@ -27,9 +46,14 @@ router.use(function(req, res, next) {
 	next();
 });
 
-router.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/client/index.html'));
-});
+
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+
+//router.get('/', function(req, res) {
+//	res.sendFile(path.join(__dirname + '/client/index.html'));
+//});
 
 //add more routes here
 
@@ -41,7 +65,7 @@ router.post('/adduser', function(req, res) {
 
 
 //register routes
-app.use('/', router);
+//app.use('/', router);
 
 //start server
 app.listen(port);
